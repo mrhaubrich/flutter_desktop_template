6c3f80c3a1945ffd171685357c06aa63b54fcd29
Marco Haubrich <marhaubrich@ucs.br>
2024-08-19 19:30:15 -0300

    first commit

---
+++ lib/src/app_module.dart
@@ -0,0 +1,15 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:nested_route/src/home_page.dart';
+
+class AppModule extends Module {
+  @override
+  void binds(i) {}
+
+  @override
+  void routes(r) {
+    r.child(
+      '/',
+      child: (context) => const HomePage(),
+    );
+  }
+}
